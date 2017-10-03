6f796a80643d3c092bc41b4a1b4b1d380d6b187d
Ticiana <Ticiana@Ticiana-NOTE1>
2017-10-03 15:26:44 -0300

    manipulação de arquivos inicial

--- src/préprocessamento/PréProcessamento.java
+++ src/préprocessamento/PréProcessamento.java
@@ -9,16 +9,57 @@ package préprocessamento;
  *
  * @author Ticiana
  */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class PréProcessamento {
+    
+   
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i;
-        // TODO code application logic here
-        for(i=0; i<10; i++)
-        { }
+        
+         BufferedReader consumoin = null ;
+         BufferedReader pessoain = null;
+         String linhapessoa,linhaconsumo;
+       
+        FileReader consumo = null;
+        try {
+            consumo = new FileReader("D:/BasesStataFINAL/consumo.csv");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PréProcessamento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        FileReader pessoas = null;
+        try {
+            pessoas = new FileReader("D:/BasesStataFINAL/pessoas.csv");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PréProcessamento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        consumoin = new BufferedReader(consumo);
+        pessoain = new BufferedReader(pessoas);
+        
+        try {
+            while ((linhapessoa = pessoain.readLine())!= null)
+            {
+                String[] splitlinhapessoa = linhapessoa.split(",");
+               
+            
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PréProcessamento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }
     
 }
